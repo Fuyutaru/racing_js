9441bc16186eeb3ba0a2bf702433cad69d17b0e9
Zijian Liu <128359977+Fuyutaru@users.noreply.github.com>
2025-02-25 11:02:12 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-    base: "/three_vite/",
+    base: "/racing_js/",
     clearScreen: false,
     optimizeDeps: {
         esbuildOptions: {
